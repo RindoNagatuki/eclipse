5d38f8c040d07069f2ebb27b55fce5e43253e257
tahara <b.tahara0916@gmail.com>
2023-09-13 11:22:31 +0900

    init

---
+++ src/main/java/com/example/app/controller/MaerkRegisterController.java
@@ -0,0 +1,13 @@
+package com.example.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/maerkRegister")
+public class MaerkRegisterController {
+
+}
