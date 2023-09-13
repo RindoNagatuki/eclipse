ad1a480a8bda767ef7d5c9bf6dae5c073e233d1b
tahara <b.tahara0916@gmail.com>
2023-09-13 13:39:22 +0900

    postmapping変更

--- src/main/java/com/example/app/controller/HomeController.java
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("home")
 public class HomeController {
 
 	public String home() {
-	return "home";
+		return "home";
+	}
+
+	@PostMapping
+	public String processRegistrationForm(
+			@RequestParam String name,
+			@RequestParam String SNSname,
+			@RequestParam String url) {
+
+		return "home";
+
 	}
 }
