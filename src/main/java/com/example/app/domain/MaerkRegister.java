8aed8d39314454991951a5e1d5bcd93d5c5aa8f8
tahara <b.tahara0916@gmail.com>
2023-09-13 11:13:37 +0900

    変数の定義

---
+++ src/main/java/com/example/app/domain/MaerkRegister.java
@@ -0,0 +1,10 @@
+package com.example.app.domain;
+
+public class MaerkRegister {
+
+	private Integer id;
+	private String name;
+	private String UesrName;
+	private String SNSname;
+	private String URL;
+}
