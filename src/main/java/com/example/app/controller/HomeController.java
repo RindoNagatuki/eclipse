96eb71db2bce0d7993a3a310b5fde2e21065f4fd
tahara <b.tahara0916@gmail.com>
2023-09-13 14:06:08 +0900

    フォーマット

--- src/main/java/com/example/app/controller/HomeController.java
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -1,23 +1,17 @@
 package com.example.app.controller;
 
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
 
 @Controller
 @RequestMapping("home")
 public class HomeController {
 
 	@GetMapping("/home")
-    public String homePage() {
-        return "home"; 
+	public String homePage() {
+		return "home";
 
 	}
 
-	
-	
 }
