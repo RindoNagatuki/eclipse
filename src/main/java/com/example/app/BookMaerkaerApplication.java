96723eff7b333dbd64892739786c46c93e714b80
tahara <b.tahara0916@gmail.com>
2023-09-13 11:05:52 +0900

    init

---
+++ src/main/java/com/example/app/BookMaerkaerApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMaerkaerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMaerkaerApplication.class, args);
+	}
+
+}
