06a6378dc6469b680ee05832e3bf10ef87e4c929
tahara <b.tahara0916@gmail.com>
2023-09-13 13:57:42 +0900

    ホーム表示のための改善試作

--- src/main/java/com/example/app/controller/HomeController.java
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.example.app.controller;
 
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 @RequestMapping("home")
 public class HomeController {
 
-	public String home() {
-		return "home";
+	@GetMapping("/home")
+    public String homePage() {
+        return "home"; 
+
 	}
 
 	
