d0898e48cb21e0f3dd8f39fd730480cef8a09324
tahara <b.tahara0916@gmail.com>
2023-09-13 13:10:18 +0900

    getMapping,postMappingの追加

--- src/main/java/com/example/app/controller/MaerkRegisterController.java
+++ src/main/java/com/example/app/controller/MaerkRegisterController.java
@@ -1,7 +1,11 @@
 package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +14,20 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/maerkRegister")
 public class MaerkRegisterController {
 
+	
+	@GetMapping("/registration")
+    public String showRegistrationForm() {
+        return "registration"; // registration.htmlへのビュー名を返します
+    }
+
+    @PostMapping("/registration")
+    public String processRegistrationForm(@RequestParam String name, 
+								    	  @RequestParam String SNSname, 
+								    	  @RequestParam String url,
+								    	  Model model) {
+        // フォームデータを処理するコードをここに追加します
+        // 例: データベースに保存する、他のサービスと連携するなど
+
+        return "redirect:/home.html"; // ホームページにリダイレクトします
+    }
 }
