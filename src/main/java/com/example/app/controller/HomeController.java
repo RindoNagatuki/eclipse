6705339e6d9f55a58d4188c0f82c2d8f3bac58d3
tahara <b.tahara0916@gmail.com>
2023-09-13 13:39:51 +0900

    ポストマッピング一時削除

--- src/main/java/com/example/app/controller/HomeController.java
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -1,9 +1,7 @@
 package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("home")
@@ -13,13 +11,6 @@ public class HomeController {
 		return "home";
 	}
 
-	@PostMapping
-	public String processRegistrationForm(
-			@RequestParam String name,
-			@RequestParam String SNSname,
-			@RequestParam String url) {
-
-		return "home";
-
-	}
+	
+	
 }
